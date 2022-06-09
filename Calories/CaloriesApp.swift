68626f3ca8bf4799e0d655bf9eb6d010f8115504
Danil Peregorodiev <thekwondoandfootbolfan@gmail.com>
2022-06-09 19:03:09 +0300

    Initial Commit

---
+++ Calories/CaloriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CaloriesApp.swift
+//  Calories
+//
+//  Created by Danil Peregorodiev on 09.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct CaloriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
